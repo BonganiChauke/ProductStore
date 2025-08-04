f4ca319afa84a8c5647d5fdc701e98acd23b81e3
BonganiChauke <bonganichauke11@outlook.com>
2025-08-04 17:28:47 +0200

    Added Required contraints for property fields

--- Models/ProductDetail.cs
+++ Models/ProductDetail.cs
@@ -9,10 +9,10 @@ namespace ProductStore.Models
         [Required, MaxLength(100)]
         public string Name { get; set; } = string.Empty;
 
-        [MaxLength(250)]
+        [Required, MaxLength(250)]
         public string? Description { get; set; } = string.Empty;
 
-        [MaxLength(100)]
+        [Required, MaxLength(100)]
         public string Brand { get; set; } = string.Empty;
 
         [Required, MaxLength(100)]
