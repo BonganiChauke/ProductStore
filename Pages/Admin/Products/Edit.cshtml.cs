4e95f47f6d5ebf5f0fb57d025b8b74321132c1d0
BonganiChauke <bonganichauke11@outlook.com>
2025-08-05 15:19:13 +0200

    Edit view model to edit product details

---
+++ Pages/Admin/Products/Edit.cshtml.cs
@@ -0,0 +1,135 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProductStore.Models;
+using ProductStore.Services;
+
+namespace ProductStore.Pages.Admin.Products
+{
+    public class EditModel : PageModel
+    {
+        //variables
+        private readonly IWebHostEnvironment environment;
+        private readonly ApplicationDbContext context;
+        public string errorMessage = "";
+        public string successMessage = "";
+
+
+        [BindProperty]
+        public ProductDetail productDetail { get; set; } = new ProductDetail();
+        public Product Product { get; set; } = new Product();
+
+        //constructor 
+        public EditModel(IWebHostEnvironment environment, ApplicationDbContext context)
+        {
+            this.environment = environment;
+            this.context = context;
+        }
+
+        public void OnGet(int? id)
+        {
+            // if the id is null
+            if(id == null)
+            {
+                // redirect the user
+                Response.Redirect("/Admin/Products/Index");
+                return;
+            }
+
+            // to read product to read from the database
+            var product = context.Products.Find(id);
+
+            // checking the product variable is null
+            if(product == null)
+            {
+                // redirect the user
+                Response.Redirect("/Admin/Products/Index");
+                return;
+            }
+
+            //initializing product variables
+            productDetail.Name = product.Name;
+            productDetail.Brand = product.Brand;
+            productDetail.Category = product.Category;
+            productDetail.Price = product.Price;
+            productDetail.Description = product.Description;
+
+            // initializing product property
+            Product = product;
+
+        }
+
+        //on post function to send to database
+        public void onPost(int? id)
+        {
+            // if the id is null
+            if (id == null)
+            {
+                // redirect the user
+                Response.Redirect("/Admin/Products/Index");
+                return;
+            }
+
+            // check if form data is valid
+            if (!ModelState.IsValid)
+            {
+                //error message
+                errorMessage = "Please provide all the required fields";
+                return;
+
+            }
+
+            // product 
+            var product = context.Products.Find(id);
+
+            // if the product is not found
+            if(product == null)
+            {
+                // redirect the user
+                Response.Redirect("/Admin/Products/Index");
+                return;
+            }
+
+            //updating the image file 
+            string newFileName = product.ImageFileName;
+
+            //if image file is not null
+            if(Product.ImageFileName != null)
+            {
+                string fileName = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+                fileName = Path.GetExtension(productDetail.ImageFile!.FileName);
+
+                string imageFullPath = environment.WebRootPath + "/products/" + fileName;
+
+                //creating a file to save the server of the new image
+                using (var stream = System.IO.File.Create(imageFullPath))
+                {
+                    productDetail.ImageFile.CopyTo(stream);
+
+                }
+
+                // delete the old image
+                string oldImageFullPath = environment.WebRootPath + "/products/" + fileName;
+                System.IO.File.Delete(oldImageFullPath);
+            }
+
+            //updating the product in the database
+            product.Name = productDetail.Name;
+            product.Brand = productDetail.Brand;
+            product.Category = productDetail.Category;
+            product.Price = productDetail.Price;
+            product.Description = productDetail.Description ?? "";
+            product.ImageFileName = newFileName;
+            context.SaveChanges();
+
+            //initializing the product property 
+            Product = product;
+
+            //success message
+            successMessage = "Product updated successfully";
+
+            // redirect the user
+            Response.Redirect("/Admin/Products/Index");
+
+        }
+    }
+}
